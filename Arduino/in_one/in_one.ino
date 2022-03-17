07f1d6e1ea76e9530484e15a83aba40e7fe49ae6
Bradley Emmons <43657042+ActivePhnx@users.noreply.github.com>
2022-03-17 11:00:55 -0400

    Complete Interrupt Setup

--- Arduino/in_one/in_one.ino
+++ Arduino/in_one/in_one.ino
@@ -7,15 +7,39 @@
 // DIO1 pin:  2
 SX1276 radio = new Module(5, 15, 4, 2);
 
-// Task Handelers
-//TaskHandle_t Task1;
-//TaskHandle_t Task2;
-
 //Button
 int in = 26;
 int out = 14;
+// Variables will change:
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
 
+// Device Specific Rescue
 String rescue = "Module ID \ Current Time \ GPS Location";
+
+// flag to indicate that a packet was received
+volatile bool receivedFlag = false;
+
+// disable interrupt when it's not needed
+volatile bool enableInterrupt = true;
+
+// this function is called when a complete packet
+// is received by the module
+// IMPORTANT: this function MUST be 'void' type
+//            and MUST NOT have any arguments!
+#if defined(ESP8266) || defined(ESP32)
+  ICACHE_RAM_ATTR
+#endif
+void setFlag(void) {
+  // check if the interrupt is enabled
+  if(!enableInterrupt) {
+    return;
+  }
+
+  // we got a packet, set the flag
+  receivedFlag = true;
+}
 
 void mit(String info){
   Serial.print(F("[SX1276] Transmitting packet ... "));
@@ -48,93 +72,41 @@ void mit(String info){
   }
 
   //Temp Testing Delay
-  delay(15000);
+  //delay(15000);
 }
 
-void lis(){
-  Serial.print(F("[SX1276] Waiting for incoming transmission ... "));
-
-  // you can receive data as an Arduino String
-  // NOTE: receive() is a blocking method!
-  //       See example ReceiveInterrupt for details
-  //       on non-blocking reception method.
-  String str;
-  int state = radio.receive(str);
-
-  if (state == RADIOLIB_ERR_NONE) {
-    // packet was successfully received
-    Serial.println(F("success!"));
-
-    // print the data of the packet
-    Serial.print(F("[SX1276] Data:\t\t\t"));
-    Serial.println(str);
-
-    // print the RSSI (Received Signal Strength Indicator)
-    // of the last received packet
-    Serial.print(F("[SX1276] RSSI:\t\t\t"));
-    Serial.print(radio.getRSSI());
-    Serial.println(F(" dBm"));
-
-    // print the SNR (Signal-to-Noise Ratio)
-    // of the last received packet
-    Serial.print(F("[SX1276] SNR:\t\t\t"));
-    Serial.print(radio.getSNR());
-    Serial.println(F(" dB"));
-
-    // print frequency error
-    // of the last received packet
-    Serial.print(F("[SX1276] Frequency error:\t"));
-    Serial.print(radio.getFrequencyError());
-    Serial.println(F(" Hz"));
-
-  } else if (state == RADIOLIB_ERR_RX_TIMEOUT) {
-    // timeout occurred while waiting for a packet
-    Serial.println(F("timeout!"));
-
-  } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
-    // packet was received, but is malformed
-    Serial.println(F("CRC error!"));
-
-  } else {
-    // some other error occurred
-    Serial.print(F("failed, code "));
-    Serial.println(state);
+void beacon(String info){
+  // Testing Beacon
+  int offset = 1;
+  int count = 0;
+  while (count < 100){
+    mit(rescue);
+    delay(120000 * offset);
+    offset += 2;
   }
+
+  // Final Beacon
+  //   int offset = 1;
+  //   int count = 0;
+      // About 3 days of beacon
+      // Starting at 6 min to 66 min intervals
+  //    while (count < 68){
+  //      mit(rescue);
+  //      delay(360000 * offset);
+  //      if (offset < 10) offset += 2;
+  //    }
+     
+   
 }
 
-void butPress(){
-  int buttonPushCounter = 0;   // counter for the number of button presses
-  int buttonState = 0;         // current state of the button
-  int lastButtonState = 0;     // previous state of the button
-
-  while (true){
-    buttonState = digitalRead(in);
-
-    if (buttonPushCounter >= 20){
-      mit(rescue);
-    }
-    if (buttonState == HIGH) {
-      // if the current state is HIGH then the button went from off to on:
-      buttonPushCounter++;
-      Serial.println("on");
-      Serial.print("number of button pushes: ");
-      Serial.println(buttonPushCounter);
-    } else {
-      // if the current state is LOW then the button went from on to off:
-      buttonPushCounter = 0;
-      lastButtonState = 0;
-    }
-    // Delay a little bit to avoid bouncing
-    delay(50);
-  }
-  
-}
 
 void setup() {
   Serial.begin(9600);
+  
   //Button Setup
   pinMode(in, INPUT);
   digitalWrite(out, HIGH);
+  pinMode(13, OUTPUT);
 
   // initialize SX1276
   Serial.print(F("[SX1276] Initializing ... "));
@@ -159,9 +131,93 @@ void setup() {
     Serial.println(F("Selected output power is invalid for this module!"));
 //    while (true);
   }
+
+  // listen interups setup
+  radio.setDio0Action(setFlag);
+  state = radio.startReceive();
+
 }
+bool light = true;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  butPress();
+  if (light){
+    digitalWrite(13, HIGH);
+    light = false;
+  } else {
+    digitalWrite(13, LOW);
+    light = true;
+  }
+
+  // Buton detection
+   buttonState = digitalRead(in);
+   if (buttonPushCounter >= 20){
+    beacon(rescue);
+   }
+   if (buttonState == HIGH) {
+     // if the current state is HIGH then the button went from off to on:
+     buttonPushCounter++;
+     Serial.println("on");
+     Serial.print("number of button pushes: ");
+     Serial.println(buttonPushCounter);
+   } else {
+     // if the current state is LOW then the button went from on to off:
+     buttonPushCounter = 0;
+     lastButtonState = 0;
+   }
+
+  if(receivedFlag) {
+    // disable the interrupt service routine while
+    // processing the data
+    enableInterrupt = false;
+
+    // reset flag
+    receivedFlag = false;
+
+    // you can read received data as an Arduino String
+    String str;
+    int state = radio.readData(str);
+
+    if (state == RADIOLIB_ERR_NONE) {
+      // packet was successfully received
+      Serial.println(F("[SX1278] Received packet!"));
+
+      // print data of the packet
+      Serial.print(F("[SX1278] Data:\t\t"));
+      Serial.println(str);
+
+      // print RSSI (Received Signal Strength Indicator)
+      Serial.print(F("[SX1278] RSSI:\t\t"));
+      Serial.print(radio.getRSSI());
+      Serial.println(F(" dBm"));
+
+      // print SNR (Signal-to-Noise Ratio)
+      Serial.print(F("[SX1278] SNR:\t\t"));
+      Serial.print(radio.getSNR());
+      Serial.println(F(" dB"));
+
+      // print frequency error
+      Serial.print(F("[SX1278] Frequency error:\t"));
+      Serial.print(radio.getFrequencyError());
+      Serial.println(F(" Hz"));
+
+    } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
+      // packet was received, but is malformed
+      Serial.println(F("[SX1278] CRC error!"));
+
+    } else {
+      // some other error occurred
+      Serial.print(F("[SX1278] Failed, code "));
+      Serial.println(state);
+
+    }
+
+    // put module back to listen mode
+    radio.startReceive();
+
+    // we're ready to receive more packets,
+    // enable interrupt service routine
+    enableInterrupt = true;
+  }
+  delay(250);
 }
